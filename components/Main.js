ae9e3963ca6aae7516b5893ba623ca0577bebeef
london-newyork <cactus1272004@yahoo.co.jp>
2021-12-20 22:43:23 +0900

    add Main Component

---
+++ components/Main.js
@@ -0,0 +1,17 @@
+import styles from '../styles/Home.module.css'
+import Links from './Links'
+import Headline from './Headline'
+
+export default function Main(props) {
+  return (
+    <div>
+      <main className={styles.main}>
+        <Headline page={props.page} number={1} array={[1,2,3]}
+        >
+        <code className={styles.code}>pages/{props.page}.js</code>
+        </Headline>
+        <Links />
+      </main>
+    </div>
+  )
+}
