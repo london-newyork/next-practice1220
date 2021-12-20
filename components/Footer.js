8cba2a9d415b21b5340255f10eb5815036710679
london-newyork <cactus1272004@yahoo.co.jp>
2021-12-20 17:22:36 +0900

    コンポーネント追加:Links Footer

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+export default function Footer() {
+    return (
+        <div>
+            <footer className={styles.footer}>
+                <p>Tanks by vercel</p>
+            </footer>
+            
+        </div>
+    )
+}
+
