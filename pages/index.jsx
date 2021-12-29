e74b1d690bff5bee6a6d5ef9c85e1923c63b8b98
london-newyork <cactus1272004@yahoo.co.jp>
2021-12-29 21:56:56 +0900

    useCallbackとuseEffect第二引数にcountを入れる

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,15 +4,18 @@ import styles from 'styles/Home.module.css'
 import Footer from 'src/components/Footer'
 import Main from 'src/components/Main'
 import Header from 'src/components/Header'
-import { useCallback, useEffect,useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 
 export default function Home() {
   const [count, setCount] = useState(1)
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1)
-  }
+  const handleClick = useCallback(() => {
+    console.log(count);
+    if (count < 10){
+      setCount((count) => count + 1)
+    }
+  },[count])
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue"
