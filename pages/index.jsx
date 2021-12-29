de5ecce3f0be810fdee5d86a2bd50c91ddfd0c39
london-newyork <cactus1272004@yahoo.co.jp>
2021-12-29 22:39:20 +0900

    handleDisplayとhandleChangeの追加

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,6 +9,8 @@ import { useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
   const [count, setCount] = useState(1)
+  const [text, setText] = useState("")
+  const [isShow, setIsShow] = useState(true)
 
   const handleClick = useCallback(() => {
     console.log(count);
@@ -24,6 +26,18 @@ export default function Home() {
     }
   },[])
 
+const handleChange = useCallback((e) => {
+  if(e.target.value.length > 5){
+    alert("５文字以内にしてください")
+    return
+  }
+  setText(e.target.value.trim())
+}, [])
+
+const handleDisplay = useCallback(() => {
+  setIsShow((isShow) => !isShow)
+}, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -32,10 +46,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <h1 className={styles.topAreaUI}>{count}</h1>
-        <button onClick={ handleClick } className={styles.topAreaUI}>
-          ボタン
-        </button>
+      <div className={styles.containerTopArea}>
+        {isShow ? <h1>{count}</h1> : null}
+          <button
+            onClick={ handleClick }
+          >
+            ボタン
+          </button>
+          <button
+            onClick={ handleDisplay }
+          >
+            {isShow ? "非表示" : "表示"}
+          </button>
+          <input type="text" value={text} onChange={ handleChange }
+          />
+      </div>
       <Main page="index"/>
       <Footer />
     </div>
