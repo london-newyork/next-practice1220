43d798426891924b6ed8173b9efd5940037fb052
london-newyork <cactus1272004@yahoo.co.jp>
2021-12-30 18:08:17 +0900

    mapとボタン操作で入力テキストを特定の場所へ吐き出す

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,11 +11,12 @@ export default function Home() {
   const [count, setCount] = useState(1)
   const [text, setText] = useState("")
   const [isShow, setIsShow] = useState(true)
+  const [array, setArray] = useState([])
 
   const handleClick = useCallback(() => {
     console.log(count);
     if (count < 10){
-      setCount((count) => count + 1)
+      setCount((prevCount) => prevCount + 1)
     }
   },[count])
 
@@ -35,8 +36,18 @@ const handleChange = useCallback((e) => {
 }, [])
 
 const handleDisplay = useCallback(() => {
-  setIsShow((isShow) => !isShow)
+  setIsShow((prevIsShow) => !prevIsShow)
 }, [])
+
+const handleAdd = useCallback(() => {
+  setArray((prevArray)=> {
+    if(prevArray.some((item)=> item === text)){
+      alert("同じ要素がすでに存在します")
+      return prevArray
+    }
+    return [...prevArray, text]
+  })
+},[text])
 
   return (
     <div className={styles.container}>
@@ -58,8 +69,15 @@ const handleDisplay = useCallback(() => {
           >
             {isShow ? "非表示" : "表示"}
           </button>
-          <input type="text" value={text} onChange={ handleChange }
-          />
+          <input type="text" value={text} onChange={ handleChange }/>
+          <button onClick={ handleAdd }>追加</button>
+          <ul>
+            {array.map((item) =>{
+              return (
+                <li key={item}>{item}</li>
+              )
+            })}
+          </ul>
       </div>
       <Main page="index"/>
       <Footer />
