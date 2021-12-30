ac1cc8b6f15793ec90e93b0c15546808d6fa695d
london-newyork <cactus1272004@yahoo.co.jp>
2021-12-30 22:51:21 +0900

    add custom hooks

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,50 +4,14 @@ import styles from 'styles/Home.module.css'
 import Footer from 'src/components/Footer'
 import Main from 'src/components/Main'
 import Header from 'src/components/Header'
-import { useCallback, useEffect, useState } from 'react'
-
+import { useCounter } from 'src/hooks/useCounter'
+import { useInputArray } from 'src/hooks/useInputArray'
+import { useBgLightBlue } from 'src/hooks/useBgLightBlue'
 
 export default function Home() {
-  const [count, setCount] = useState(1)
-  const [text, setText] = useState("")
-  const [isShow, setIsShow] = useState(true)
-  const [array, setArray] = useState([])
-
-  const handleClick = useCallback(() => {
-    console.log(count);
-    if (count < 10){
-      setCount((prevCount) => prevCount + 1)
-    }
-  },[count])
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue"
-    return () => {
-      document.body.style.backgroundColor = ""
-    }
-  },[])
-
-const handleChange = useCallback((e) => {
-  if(e.target.value.length > 5){
-    alert("５文字以内にしてください")
-    return
-  }
-  setText(e.target.value.trim())
-}, [])
-
-const handleDisplay = useCallback(() => {
-  setIsShow((prevIsShow) => !prevIsShow)
-}, [])
-
-const handleAdd = useCallback(() => {
-  setArray((prevArray)=> {
-    if(prevArray.some((item)=> item === text)){
-      alert("同じ要素がすでに存在します")
-      return prevArray
-    }
-    return [...prevArray, text]
-  })
-},[text])
+  const { count, isShow, handleClick, handleDisplay } = useCounter()
+  const {text, array, handleChange, handleAdd } = useInputArray()
+  useBgLightBlue()
 
   return (
     <div className={styles.container}>
---
+++ src/hooks/useBgLightBlue.js
@@ -0,0 +1,10 @@
+import { useCallback, useEffect, useState } from 'react'
+
+export const useBgLightBlue = () => {
+    useEffect(() => {
+      document.body.style.backgroundColor = "lightblue"
+      return () => {
+        document.body.style.backgroundColor = ""
+      }
+    },[])
+  }
